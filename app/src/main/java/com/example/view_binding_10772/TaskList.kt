48349b46980cc1509a3454f3bd3781c7547476f9
ASUS <drl.yk99@gmail.com>
2022-09-15 17:20:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10772/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.view_binding_10772
+
+object TaskList {
+    val taskList = listOf<Task> (
+        Task("Chapter 1", "Bangun Tidur",
+            "Kebiasaan sehari-hari"),
+        Task("Chapter 2", "Kerja Rodi di Fifa", "Biar engga di kick dari Club"),
+        Task("Chapter 3", "Ngeteh", "Biar fokus sambil ngeteh"),
+        Task("Chapter 4", "Dimana Ronaldo", "Mau gua bantai"),
+        Task("Chapter 5", "Upload di Instragam",
+            "Buat Buktiin Ronaldo Cupu")
+    )
+}
